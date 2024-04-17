f145d9d85316a418d201e39ffb6621652768973f
Rodrigo Cirino <rodrigocirinocl@gmai.com>
2024-04-17 00:03:51 -0300

    first commit

---
+++ calculadora_imc/src/components/ImcTable.jsx
@@ -0,0 +1,34 @@
+import "./ImcTable.css"
+import React from 'react'
+import Button from './Button'
+
+const ImcTable = ({ data, imc, info, infoClass, resetCalc }) => {
+    return (
+        <div id='result-container'>
+            <p id='imc-number'>
+                Seu IMC: <span className={infoClass}>{imc}</span>
+            </p>
+            <p id='imc-info'>
+                Situação atual: <span className={infoClass}>{info}</span>
+            </p>
+            <h3>Confira as classificações:</h3>
+            <div className="imc-table">
+            <div className="table-header">
+                <h4>IMC</h4>
+                <h4>Classificações</h4>
+                <h4>Obesidade</h4>
+            </div>
+            {data.map((item) => (
+                <div className="table-data" key={item.info}>
+                    <p>{item.classification}</p>
+                    <p>{item.info}</p>
+                    <p>{item.obesity}</p>
+                </div>
+                 ))}
+            </div>
+            <Button id="back-btn" text="Voltar" action={resetCalc}/>
+        </div>
+    )
+}
+
+export default ImcTable
